668999fef92ac5a44427c5911e812f62f500eeff
Pablo Vicente Munuera <pablovm1990@gmail.com>
2017-03-10 15:21:51 +0100

    almost done

--- src/recognizeEveryCellInTheSequence.m
+++ src/recognizeEveryCellInTheSequence.m
@@ -5,10 +5,22 @@ function [ recognizedCells ] = recognizeEveryCellInTheSequence( sequenceFile )
     recognizedCells = {};
     load(sequenceFile);
     
-    imagesOfSerieByChannelCh0 = imagesOfSerieByChannel{:, 1};
+    imagesOfSerieByChannelCh0 = imagesOfSerieByChannel(:, 2);
     
-    for i = 1:length(imagesOfSerieByChannelCh0)
-        cells = regionProps(imagesOfSerieByChannelCh0{i});
+    for numImg = 1:length(imagesOfSerieByChannelCh0)
+        img = imagesOfSerieByChannelCh0{numImg};
+        imgDilatted = imdilate(img, strel('disk', 20));
+        imgBinary = im2bw(imgDilatted, 0.2);
+        cells = regionprops(imgBinary, 'all');
+        cellAreas = vertcat(cells.Area);
+        cellsRejected = cells(cellAreas <= 9500);
+        cells = cells(cellAreas > 9500);
+        imgBinaryNoSmallCells = imgBinary;
+        for numCell = 1:length(cellsRejected)
+            imgBinaryNoSmallCells(cellsRejected(numCell).PixelList(:, 2), cellsRejected(numCell).PixelList(:, 1)) = 0;
+        end
+        imshow(imgBinaryNoSmallCells);
+        recognizedCells(end+1) = {cells};
     end
 end
 
