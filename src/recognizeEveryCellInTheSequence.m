c482edfe833fdf15f37793ac09a42e4b5dd52191
Pablo Vicente Munuera <pablovm1990@gmail.com>
2017-03-10 17:38:27 +0100

    corresponding cells done

--- src/recognizeEveryCellInTheSequence.m
+++ src/recognizeEveryCellInTheSequence.m
@@ -2,6 +2,7 @@ function [ recognizedCells ] = recognizeEveryCellInTheSequence( sequenceFile, di
 %RECOGNIZEEVERYCELLINTHESEQUENCE Summary of this function goes here
 %   Detailed explanation goes here
     MIN_SIZE_CELL = 4500;
+    MAX_DST_FRAMES = 2;
     recognizedCells = {};
     imgsFinal = {};
     load(sequenceFile);
@@ -23,7 +24,6 @@ function [ recognizedCells ] = recognizeEveryCellInTheSequence( sequenceFile, di
         cellAreas = vertcat(cells.Area);
         cellsRejected = cells(cellAreas <= MIN_SIZE_CELL);
         cells = cells(cellAreas > MIN_SIZE_CELL);
-        cells.frame = numImg;
         imgBinaryNoSmallCells = imgFilled;
         for numCell = 1:length(cellsRejected)
             imgBinaryNoSmallCells(cellsRejected(numCell).PixelList(:, 2), cellsRejected(numCell).PixelList(:, 1)) = 0;
@@ -35,26 +35,34 @@ function [ recognizedCells ] = recognizeEveryCellInTheSequence( sequenceFile, di
     save(strcat(directory, 'recognizedCells'), 'recognizedCells', 'imgBinaryNoSmallCells');
     
     %Relate each file with its correspondence on the other frames
+    cellFrames = cellfun(@(x, y) ones(length(x) , 1)*y, recognizedCells, mat2cell(1:length(imagesOfSerieByChannelCh0), 1, ones(length(imagesOfSerieByChannelCh0), 1)), 'UniformOutput', false);
+    cellFrames = vertcat(cellFrames{:});
     cellsFound = vertcat(recognizedCells{:});
     totalCellsFound = length(cellsFound);
     %First number: label, second number: frameNumber
-    correspondingCells = zeros(totalCellsFound, 2);
+    correspondingCells = zeros(totalCellsFound, 1);
     
     actualLabelOfCell = 1;
     for numCellActual = 1:totalCellsFound
-        if correspondingCells == 0
-            actualCell = recognizedCells{numCellActual};
-            correspondingCells(numCellActual, 1) = actualLabelOfCell;
-            correspondingCells(numCellActual, 2) = actualCell.frame;
+        if correspondingCells(numCellActual) == 0
+            actualCell = cellsFound(numCellActual);
+            correspondingCells(numCellActual) = actualLabelOfCell;
             for numCellToRecognized = 2:totalCellsFound
-                if numCellToRecognized ~= numCellActual
-                    cellToRecognized = recognizedCells{numCellActual};
-                    
+                if numCellToRecognized ~= numCellActual && correspondingCells(numCellToRecognized) == 0
+                    cellToRecognized = cellsFound(numCellToRecognized);
+                    if (length(intersect(actualCell.PixelList, cellToRecognized.PixelList, 'rows')) > MIN_SIZE_CELL/2)
+                        correspondingCells(numCellToRecognized) = actualLabelOfCell;
+                    end
                 end
             end
             
             actualLabelOfCell = actualLabelOfCell + 1;
         end
+    end
+    
+    correspondingCells
+    for actualLabel = 1:(actualLabelOfCell-1)
+        cellFrames(correspondingCells == actualLabel)
     end
 end
 
