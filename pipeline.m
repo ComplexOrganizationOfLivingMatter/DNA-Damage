62bd71dd40f2a62921b08ec329b9aa1ca9f4821b
Pablo Vicente Munuera <pablovm1990@gmail.com>
2017-03-21 15:04:10 +0100

    change the last file created

--- pipeline.m
+++ pipeline.m
@@ -36,7 +36,7 @@ function [] = pipeline( )
         load(strcat(directory, '\recognizedCells.mat'));
         
         for numCell = 1:length(infoCells)
-            firstOuputFile = strcat(directory, '\', 'Cell_', num2str(numCell), '_', 'Proyeccion_General_3D_FOCI-VERDE-2.tiff');
+            firstOuputFile = strcat(directory, '\', 'Cell_', num2str(numCell), '_', '_networkInfo.mat');
             if exist(firstOuputFile, 'file') ~= 2
                 rect = infoCells{numCell, 1};
                 rect(rect<1) = 1;
