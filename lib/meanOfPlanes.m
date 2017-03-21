a38a73ea1fd68e17fbe74261a7ad5b7f3e829979
Pablo Vicente Munuera <pablovm1990@gmail.com>
2017-03-21 11:38:08 +0100

    refactor

--- lib/meanOfPlanes.m
+++ lib/meanOfPlanes.m
@@ -4,15 +4,7 @@ function [ centroids ] = meanOfPlanes( cellsInfo )
     centroids = zeros(length(cellsInfo), 3);
     for numCellFound = 1:length(cellsInfo)
         cellFound = cellsInfo{numCellFound};
-        cellPlanes = unique(cellFound(:, 3));
-        actualCentroid = zeros(length(cellPlanes), 2);
-        for cellPlane = 1:length(cellPlanes)
-            cellFoundInPlane = cellFound(cellFound(:, 3) == cellPlanes(cellPlane), 1:2);
-            if ~isempty(cellFoundInPlane)
-                actualCentroid(cellPlane, :) = mean(cellFoundInPlane);
-            end
-        end
-        centroids(numCellFound, :) = [mean(actualCentroid, 1), mean(cellFound(:, 3))];
+        centroids(numCellFound, :) = mean(cellFound(:, :), 1);
     end
 end
 
