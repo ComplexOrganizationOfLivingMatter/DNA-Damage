920431b2303658098e1be5f77756fc86ac142c74
Pablo Vicente Munuera <pablovm1990@gmail.com>
2017-03-21 11:41:51 +0100

    distance between foci and heterochromatin done

--- src/getNetworkInfo.m
+++ src/getNetworkInfo.m
@@ -10,5 +10,17 @@ function [ ] = getNetworkInfo(directory, numCell)
     
     distanceBetweenFoci = squareform(pdist(centroidsFoci));
     distanceFociVsHeterochromatin = pdist2(centroidsFoci, centroidsHeterochromatin);
+    fociToCompare = repmat(centroidsFoci(:, 3), 1, size(centroidsHeterochromatin, 1));
+    heterochromatinToCompare = repmat(centroidsHeterochromatin(:, 3), 1, size(centroidsFoci, 1));
+    
+    fociAboveHeterochromatin = fociToCompare > heterochromatinToCompare';
+    fociAboveHeterochromatin(fociAboveHeterochromatin == 0) = -1;
+    
+    %We also take into accout if the focis is above or below
+    %heterochromatin. When the sign is negative (< 0) it is below. Above
+    %otherwise
+    distanceFociVsHeterochromatin = distanceFociVsHeterochromatin .* fociAboveHeterochromatin;
+    
+    
 end
 
