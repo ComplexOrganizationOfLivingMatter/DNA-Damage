7eb4787dc09a8d78169d755dae5187edec236985
Pablo Vicente Munuera <pablovm1990@gmail.com>
2017-03-21 12:12:18 +0100

    refactoring and getting it fixed

--- lib/getConnectedGraphWithMinimumDistances.m
+++ lib/getConnectedGraphWithMinimumDistances.m
@@ -4,14 +4,10 @@ function [ adjacencyMatrix ] = getConnectedGraphWithMinimumDistances( distanceBe
 %
 %   Developed by Pablo Vicente-Munuera
 
-    distanceObjectsRemaining = squareform(distanceBetweenObjects);
+    distanceObjectsRemaining = distanceBetweenObjects;
     distanceObjectsRemaining(logical(eye(size(distanceObjectsRemaining)))) = intmax('int16');
     connectedComps = 2;
-    i = 1;
     while connectedComps > 1
-        if i > size(adjacencyMatrix,1)
-            i=1;
-        end
         minimumDistance = min(distanceObjectsRemaining(:));
         [rowMin, colMin] = find(distanceObjectsRemaining == minimumDistance, 1);
         adjacencyMatrix(rowMin, colMin) = distanceObjectsRemaining(rowMin, colMin);
@@ -20,17 +16,11 @@ function [ adjacencyMatrix ] = getConnectedGraphWithMinimumDistances( distanceBe
 
         distanceObjectsRemaining(rowMin, colMin) = intmax('int32');
         distanceObjectsRemaining(colMin, rowMin) = intmax('int32');
-
-        contConnectedComps = contConnectedComps - 1;
-        if contConnectedComps <= 0
-            connectedComps = graphconncomp(adjacencyMatrix, 'Directed', 'false');
-            if connectedComps == 1
-                return
-            end
-            contConnectedComps = connectedComps;
+        
+        connectedComps = graphconncomp(adjacencyMatrix, 'Directed', 'false');
+        if connectedComps == 1
+            return
         end
-
-        i = i + 1;
     end
 end
 
