3bd8125d3a66cb625ff51e4dacbade27ba28f1ae
Pablo Vicente Munuera <pablovm1990@gmail.com>
2017-04-10 17:06:18 +0200

    done

--- src/analyzeNetworksAndGetCharacteristics.m
+++ src/analyzeNetworksAndGetCharacteristics.m
@@ -52,49 +52,9 @@ function [ ] = analyzeNetworksAndGetCharacteristics()
     IR_MinDistancesOfFociToHeterocrhomatin = horzcat(allMinDistacesOfFociToHeterochromatin{1, :});
     VP16_MinDistancesOfFociToHeterocrhomatin = horzcat(allMinDistacesOfFociToHeterochromatin{2, :});
     
-    emptyCells = cellfun(@(x) ~isempty(x), allMinDistacesOfFociToHeterochromatin);
-    frequenciesIR = cellfun(@(x) histcounts(x, 12, 'BinLimits', [0,3]), allMinDistacesOfFociToHeterochromatin(1, emptyCells(1, :)), 'UniformOutput', false);
-    frequenciesVP16 = cellfun(@(x) histcounts(x, 12, 'BinLimits', [0,3]), allMinDistacesOfFociToHeterochromatin(2, emptyCells(2, :)), 'UniformOutput', false);
+    paintHistogramOfMeansPerNuclei(allMinDistacesOfFociToHeterochromatin);
     
-    listDistributionIR = cellfun(@(x) x/sum(x), frequenciesIR, 'UniformOutput', false);
-    listDistributionVP16 = cellfun(@(x) x/sum(x), frequenciesVP16, 'UniformOutput', false);
-    
-    meanListDistributionIR = mean(vertcat(listDistributionIR{:}));
-    meanListDistributionVP16 = mean(vertcat(listDistributionVP16{:}));
-    stdeviationVP16 = std(vertcat(listDistributionVP16{:}));
-    stdeviationIR = std(vertcat(listDistributionIR{:}));
-    standarderrorIR = stdeviationIR ./ sqrt(length(vertcat(listDistributionIR{:})));
-    standarderrorIR(isnan(standarderrorIR)) = 0;
-    standarderrorVP16 = stdeviationVP16 ./ sqrt(length(vertcat(listDistributionVP16{:})));
-    standarderrorVP16(isnan(standarderrorVP16)) = 0;
-    
-    
-    irMinsPerNuclei = vertcat(allMinDistacesOfFociToHeterochromatin(1, :))';
-    vp16MinsPerNuclei = vertcat(allMinDistacesOfFociToHeterochromatin(2, :))';
-    
-    irMeanMinsPerNuclei = cellfun(@(x) mean(x), irMinsPerNuclei);
-    vp16MeanMinsPerNuclei = cellfun(@(x) mean(x), vp16MinsPerNuclei);
-    
-    [hTTestMinPerNuclei, pValueTTestMinPerNuclei] = ttest2(irMeanMinsPerNuclei(~isnan(irMeanMinsPerNuclei)), vp16MeanMinsPerNuclei(~isnan(vp16MeanMinsPerNuclei)));
-    
-     figMin = figure; barMin = bar([meanListDistributionVP16', meanListDistributionIR']);
-    figMin.Colormap = colormap('copper');
-    hold on;
-    errorbar([0.85:11.85; 1.15:12.15]', [meanListDistributionVP16', meanListDistributionIR'], [standarderrorVP16', standarderrorIR'],'r.')
-    legend('VP16', 'IR');
-    ylabel('Percentage of foci');
-    xlabel('Min distance to the closest heterochromatin')
-    set(gca, 'XTick', 1:12, 'XTickLabel', {'0 - 0.25'; '0.25 - 0.5'; '0.5 - 0.75'; '0.75 - 1'; '1 - 1.25'; '1.25 - 1.5'; '1.5 - 1.75'; '1.75 - 2'; '2 - 2.25'; '2.25 - 2.5'; '2.5 - 2.75'; '2.75 - 3'});
-    
-    irMeansPerNuclei = vertcat(allMeanDistacesOfFociToHeterochromatin(1, :))';
-    vp16MeansPerNuclei = vertcat(allMeanDistacesOfFociToHeterochromatin(2, :))';
-    
-    irMeanMeansPerNuclei = cellfun(@(x) mean(x), irMeansPerNuclei);
-    vp16MeanMeansPerNuclei = cellfun(@(x) mean(x), vp16MeansPerNuclei);
-    
-    [hTTestMeanPerNuclei, pValueTTestMeanPerNuclei] = ttest2(irMeanMeansPerNuclei(~isnan(irMeanMeansPerNuclei)), vp16MeanMeansPerNuclei(~isnan(vp16MeanMeansPerNuclei)));
- 
-   
+    paintHistogramOfMeansPerNuclei(allMeanDistacesOfFociToHeterochromatin);
     
 %     [h, p] = ttest2(IR_MinDistancesOfFociToHeterocrhomatin, VP16_MinDistancesOfFociToHeterocrhomatin, 'Vartype','unequal')
 %     
