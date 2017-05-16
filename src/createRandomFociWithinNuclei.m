b66c11a3edec6d263e4175aafc50058b8af91a13
Pablo Vicente Munuera <pablovm1990@gmail.com>
2017-05-16 13:08:48 +0200

    creating the possible pixels of all the layers of the cell

--- src/createRandomFociWithinNuclei.m
+++ src/createRandomFociWithinNuclei.m
@@ -1,11 +1,11 @@
-function [  ] = createRandomFociWithinNuclei( )
+function [  ] = createRandomFociWithinNuclei(maxRandomization)
 %CREATERANDOMFOCIWITHINNUCLEI Summary of this function goes here
 %   Detailed explanation goes here
     
     outputDirectory = 'results\randomization';
     mkdir(outputDirectory);
     allFiles = getAllFiles('results\segmentation\');
-    onlyPixelsFiles = cellfun(@(x) isempty(strfind(x, 'results.mat')) == 0, allFiles);
+    onlyPixelsFiles = cellfun(@(x) isempty(strfind(x, 'Pixels')) == 0, allFiles);
     pixelsFiles = allFiles(onlyPixelsFiles);
     for numFile = 1:size(pixelsFiles, 1)
         fullPathFile = pixelsFiles{numFile};
@@ -18,7 +18,7 @@ function [  ] = createRandomFociWithinNuclei( )
         infoCell = importdata(strcat(directory, '\Cell_', numCell, '_results.mat'));
         centroidsFoci = infoCell.Datos{1,1};
         centroidsFoci = vertcat(centroidsFoci{:, 2});
-        numberOfFoci = size(centroidsFoci, 1);
+        totalFoci = size(centroidsFoci, 1);
         clear infoCell centroidsFoci;
         
         %We need the area of the cell
@@ -28,10 +28,26 @@ function [  ] = createRandomFociWithinNuclei( )
         %area where can fall the random foci
         load(fullPathFile);
         
+        [pixelsXArea, pixelsYArea] = find(proyb_rect > 0);
         
+        finalPixelArea = [];
+        for i = 1:size(mask_Hetero, 2)
+            actualAreaOfLayer = [pixelsXArea, pixelsYArea];
+            [pixelXheteroOfLayer, pixelYheteroOfLayer] = find(mask_Hetero{i});
+            actualPixelArea = actualAreaOfLayer(ismember(actualAreaOfLayer, [pixelXheteroOfLayer, pixelYheteroOfLayer], 'rows') == 0, :);
+            actualPixelArea(:, 3) = i;
+            finalPixelArea = vertcat(finalPixelArea, actualPixelArea);
+        end
         
         %We already have all the info we need
         %Do the randomization
+        for numRandom = 1:maxRandomization
+            for numFoci = 1:totalFoci
+                
+                %Remove also the possible pixels of the same Z
+                
+            end
+        end
         
     end
 end
