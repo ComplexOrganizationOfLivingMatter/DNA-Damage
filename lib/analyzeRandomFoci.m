675eac4ecf0f996974f6c09aaf02c3ca77952263
Pablo Vicente Munuera <pablovm1990@gmail.com>
2017-05-17 12:47:40 +0200

    doing it properly

--- lib/analyzeRandomFoci.m
+++ lib/analyzeRandomFoci.m
@@ -2,28 +2,41 @@ function [ ] = analyzeRandomFoci( )
 %ANALYZERANDOMFOCI Summary of this function goes here
 %   Detailed explanation goes here
 
+    randomFiles = getAllFiles('D:\Pablo\DNA-Damage\results\randomization');
     
-    load(strcat(directory, '\recognizedCells.mat'));
-    rect = finalCells{str2num(numCell), 1};
-    rect(rect<1) = 1;
-    if rect(1) + rect(3) >= size(imgBinaryNoSmallCells, 1)
-        rect(3) = size(imgBinaryNoSmallCells, 1) - 1 - rect(1);
-    end
-    if rect(2) + rect(4) >= size(imgBinaryNoSmallCells, 2)
-        rect(4) = size(imgBinaryNoSmallCells, 2) - 1 - rect(2);
-    end
-    
-    getUmFromPixels(randomCentroids, rect);
-    
-    [ fociClusters ] = createFociClustersOfHeterochromatin( randomCentroids, num_hetero_um);
-    
-        if ~isempty(strfind(resultFiles{numFile}, 'IR_30min'))
-            allMinDistacesOfFociToHeterochromatin{1, end+1} = vertcat(fociClusters{:, 2})';
-            allMeanDistacesOfFociToHeterochromatin{1, end+1} = vertcat(fociClusters{:, 3})';
-        else
-            allMinDistacesOfFociToHeterochromatin{2, end+1} = vertcat(fociClusters{:, 2})';
-            allMeanDistacesOfFociToHeterochromatin{2, end+1} = vertcat(fociClusters{:, 3})';
+    for numRandomFile = 1:size(randomFiles, 1)
+        fullPathFile = randomFiles{numRandomFile};
+        directory = strrep(fullPathFile, 'randomization', 'segmentation');
+        directorySplitted = strsplit(directory, '\');
+        directory = strjoin(directorySplitted(1:end-1), '\');
+        nameFile = directorySplitted{end};
+        nameFileSplitted = strsplit(nameFile, '_');
+        numCell = nameFileSplitted{end};
+        numCell = numCell(1:end-4);
+        
+        load(strcat(directory, '\recognizedCells.mat'));
+        rect = finalCells{str2num(numCell), 1};
+        rect(rect<1) = 1;
+        if rect(1) + rect(3) >= size(imgBinaryNoSmallCells, 1)
+            rect(3) = size(imgBinaryNoSmallCells, 1) - 1 - rect(1);
         end
+        if rect(2) + rect(4) >= size(imgBinaryNoSmallCells, 2)
+            rect(4) = size(imgBinaryNoSmallCells, 2) - 1 - rect(2);
+        end
+
+        getUmFromPixels(randomCentroids, rect);
+
+        [ fociClusters ] = createFociClustersOfHeterochromatin( randomCentroids, num_hetero_um);
+
+            if ~isempty(strfind(resultFiles{numFile}, 'IR_30min'))
+                allMinDistacesOfFociToHeterochromatin{1, end+1} = vertcat(fociClusters{:, 2})';
+                allMeanDistacesOfFociToHeterochromatin{1, end+1} = vertcat(fociClusters{:, 3})';
+            else
+                allMinDistacesOfFociToHeterochromatin{2, end+1} = vertcat(fociClusters{:, 2})';
+                allMeanDistacesOfFociToHeterochromatin{2, end+1} = vertcat(fociClusters{:, 3})';
+            end
+            
+    end
     
     %Get the relevant info after all the compilation
     [hTTestMinPerNuclei, pValueTTestMinPerNuclei] = paintHistogramOfMeansPerNuclei(allMinDistacesOfFociToHeterochromatin, 'minDistanceToHeterochromatin_Random', 12, [0,3]);
